9552f9c762f7f7f2398a75ccef3e4728ec1fccb7
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-08-13 22:11:19 -0500

    create a file in /tmp using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Create file in /tmp
+
+file { '/tmp/holberton':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/holberton',
+}
