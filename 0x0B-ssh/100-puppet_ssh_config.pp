7294e6f2d40cdb27f8de51e2c21ba1dab399d448
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-08-15 23:44:58 -0500

    make changes to our configuration file, connect to a server without typing a password

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,5 @@
+# puppet to make changes to our configuration file
+exec { 'ssh_config':
+  path    => '/bin',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
+}
