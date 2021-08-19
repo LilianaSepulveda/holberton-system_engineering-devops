5489cdcb7d7b7a42ff17f54df7a004436270105a
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-08-18 23:46:25 -0500

    configure server nginx with puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+# configue server Nginx with puppet
+
+package { 'nginx':
+  ensure   => 'latest',
+  name     => 'nginx',
+  provider => 'apt'
+}
+
+file { 'index':
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School'
+}
+
+file_line { '301 Moved Permanently':
+  path  => '/etc/nginx/sites-available/default',
+  line  => '\trewrite ^/redirect_me https://twitter.com/LiliSepulveda13 permanent;',
+  after => '^server {'
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true
+}
